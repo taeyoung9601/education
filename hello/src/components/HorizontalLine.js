fa79ae7a2a0ec9ab0f03ce2c07d9efe4ffe6cef5
taeyoung9601 <168715658+taeyoung9601@users.noreply.github.com>
2024-11-12 18:09:21 +0900

    Delete hello directory

--- hello/src/components/HorizontalLine.js
+++
@@ -1,9 +0,0 @@
-console.groupCollapsed('src/components/HorizontalLine.js'); console.groupEnd();
-
-function HorizontalLine() {
-    console.groupCollapsed('HorizontalLine() invoked.'); console.groupEnd();
-
-    return <hr/>;
-}
-
-export default HorizontalLine;
