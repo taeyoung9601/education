91a8650b50e9dfaa733d2df1c082276214052216
taeyoung9601 <168715658+taeyoung9601@users.noreply.github.com>
2024-12-06 17:59:40 +0900

    Add files via upload

---
+++ java/eclipse/jse/Sample42.java
@@ -0,0 +1,14 @@
+
+public class Sample42 {
+
+	// 목표: 클래스 선언 시, 생성자로 초기화되지 않은
+	// 필드에 기본 값을 확인(자바 타입별 기본 값)
+	public static void main(String[] args) {
+		DefaultFieldValue obj = new DefaultFieldValue();
+		
+		Utils.printToConsole(obj);
+		
+		
+	}//main
+	
+}//end class
---
+++ java/eclipse/jse/Sample45.java
@@ -0,0 +1,28 @@
+
+public class Sample45 {
+
+	public static void main(String[] args) {
+		
+		House house1 = new House();
+		Utils.printToConsole(++House.몇채);
+		
+		house1.보일러on();  //OK. 인스턴스 메소드
+//		House.보일러on();   // XX, 모든 객체에 공유되는 메소드(기능)
+							// 	   결국 이 메소드가 호출 가능하다면, 동시에
+							//     모든 객체(집)의 보일러가 켜지는거죠..
+		
+		//================
+		
+		House house2 = new House();
+		Utils.printToConsole(++House.몇채);
+		
+		
+		//================
+		
+		House house3 = new House();
+		Utils.printToConsole(++House.몇채);
+		
+
+	}
+
+}
