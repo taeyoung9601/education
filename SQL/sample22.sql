349ad60cd16c73af1515a9da4714f221026f815b
taeyoung9601 <168715658+taeyoung9601@users.noreply.github.com>
2025-01-08 18:14:27 +0900

    Update sample22.sql

--- SQL/sample22.sql
+++ SQL/sample22.sql
@@ -53,3 +53,237 @@ VALUES (12,'인사'); --ORA-00947: not enough values
 INSERT INTO dept(deptno,dname,loc)
 VALUES ('개발', 13, '인사');  --ORA-01722: invalid number
 
+
+
+
+
+
+DROP TABLE mydept PURGE;
+
+
+CREATE TABLE mydept
+AS
+SELECT * FROM dept
+WHERE 1 = 2;        -- 같을 수가 없다 => 기존 테이블의 스키마만 복사(데이터 제외) 
+
+DESC mydept;
+
+SELECT 
+    *
+FROM
+    mydept;
+
+
+CREATE TABLE mydept2
+AS
+SELECT 
+    * 
+FROM
+    dept;
+
+SELECT 
+    * 
+FROM 
+    dept;
+
+
+INSERT INTO mydept
+SELECT
+    deptno,
+    dname,
+    loc
+FROM
+    dept;
+
+SHOW autocommit;
+set autocommit off; 
+
+INSERT INTO mydept
+VALUES (77,'임시', '안산');
+
+DELETE FROM mydept
+WHERE deptno = 77;
+
+COMMIT;
+
+
+
+
+CREATE TABLE myemp_hire AS
+SELECT
+    empno,
+    ename,
+    hiredate,
+    sal
+FROM
+    emp
+WHERE
+    1 =2;
+
+DESC myemp_hire;
+
+-- ------------------------------------------------------
+
+DROP TABLE myemp_mgr;
+
+
+CREATE TABLE myemp_mgr AS      -- 2nd. Table creation.
+SELECT
+    empno,
+    ename,
+    mgr
+FROM
+    emp
+WHERE
+    1 = 2;        -- 기존 테이블의 스키마만 복사(데이터 제외)
+
+
+DESC myemp_mgr;
+
+-- ------------------------------------------------------
+SET AUTOCOMMIT ON;
+SHOW AUTOCOMMIT;
+
+INSERT ALL
+    INTO myemp_hire VALUES (empno, ename, hiredate, sal)
+    INTO myemp_mgr  VALUES (empno, ename, mgr)
+
+    SELECT
+        empno,
+        ename,
+        hiredate,
+        sal,
+        mgr
+    FROM
+        emp;
+
+-- ------------------------------------------------------
+SELECT
+    *
+FROM
+    myemp_hire;
+
+
+SELECT
+    *
+FROM
+    myemp_mgr;
+
+
+-- ------------------------------------------------------
+DROP TABLE myemp_hire2;
+
+
+CREATE TABLE myemp_hire2 AS   -- 1st. Table creation.
+SELECT
+    empno,
+    ename,
+    hiredate,
+    sal
+FROM
+    emp
+WHERE
+    1 = 2;        -- 기존 테이블의 스키마만 복사(데이터 제외)
+
+
+DESC myemp_hire2;
+
+-- ------------------------------------------------------
+
+DROP TABLE myemp_mgr2;
+
+
+CREATE TABLE myemp_mgr2 AS    -- 2nd. Table creation.
+SELECT
+    empno,
+    ename,
+    mgr
+FROM
+    emp
+WHERE
+    1 = 2;        -- 기존 테이블의 스키마만 복사(데이터 제외)
+
+
+DESC myemp_mgr2;
+
+
+-- ------------------------------------------------------
+
+
+INSERT ALL
+    WHEN sal > 3000 THEN
+        INTO myemp_hire2  VALUES (empno, ename, hiredate, sal)
+
+    WHEN mgr = 7698 THEN
+        INTO myemp_mgr2   VALUES (empno,ename,mgr)
+
+    SELECT
+        empno,
+        ename,
+        hiredate,
+        sal,
+        mgr
+    FROM
+        emp;
+
+-- ------------------------------------------------------
+
+SELECT
+    *
+FROM
+    myemp_hire2;
+
+
+SELECT
+    *
+FROM
+    myemp_mgr2;
+
+---------------------------------------------------------------
+
+CREATE TABLE myemp_hire3 AS
+SELECT
+    empno,
+    ename,
+    hiredate,
+    sal
+FROM
+    emp
+WHERE
+    1 =2;
+
+DESC myemp_hire3;
+
+------------------------------------
+
+CREATE TABLE myemp_mgr3 AS
+SELECT
+    empno,
+    ename,
+    mgr
+FROM
+    emp
+WHERE
+    1 = 2;
+
+DESC myemp_mgr3;
+
+
+
+---------------------------------------
+INSERT FIRST
+    WHEN sal = 800 THEN
+        INTO myemp_hire3 VALUES (empno, ename, hiredate, sal)
+
+    WHEN sal < 2500 THEN
+        INTO myemp_mgr3 VALUES (empno, ename, mgr)
+
+    SELECT
+        empno,
+        ename,
+        hiredate,
+        sal,
+        mgr
+    FROM
+        emp;
+
